ddaa9242b08d29a939756edec896c34aec1853a6
Malaika Waris <MalaikaWaris1@gmail.com>
2026-08-01 11:04:52 +0500

    url chnages

--- my-react-app/src/api/axios.js
+++ my-react-app/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://kbeauty-backend-r0mz.onrender.com",
   withCredentials: true,
 });
 
